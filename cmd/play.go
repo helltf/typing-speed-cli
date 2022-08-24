667813360f399f2eb7a080ff139cb06d84834235
helltf <Dario.Zachow5530@gmail.com>
2022-08-24 20:15:44 +0200

    feat(play): creating function to get key strokes

--- cmd/play.go
+++ cmd/play.go
@@ -1,12 +1,12 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/eiannone/keyboard"
 	"github.com/spf13/cobra"
 )
 
@@ -14,22 +14,41 @@ import (
 var playCmd = &cobra.Command{
 	Use:   "play",
 	Short: "Play a game to test you typing speed",
-	Long: ``,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("play called")
+		keyStrokes := make(chan string)
+		// var exampleContext = "this is an example"
+		go getKeys(keyStrokes)
+
+		for key := range keyStrokes {
+			fmt.Printf("%v\n", key)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(playCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func getKeys(c chan<- string) {
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// playCmd.PersistentFlags().String("foo", "", "A help for foo")
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			panic(err)
+		}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// playCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+		if key == keyboard.KeyEsc {
+			close(c)
+			break
+		}
+
+		c <- string(char)
+	}
 }
