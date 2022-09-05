13306922fe4b7bac38f978a1bf0f268b05b8d0d9
DarioZachow <dario.zachow@deutschebahn.com>
2022-09-05 09:03:49 +0200

    chore: reverting changes made to context and main

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/helltf/typing-speed-cli/cmd"
+import (
+	"github.com/helltf/typing-speed-cli/cmd"
+)
 
 func main() {
 	cmd.Execute()
