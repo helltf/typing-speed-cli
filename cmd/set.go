d46fd764b86cf8186aeeacf85f04f53bfd1b8732
DarioZachow <dario.zachow@deutschebahn.com>
2022-09-15 09:36:19 +0200

    feat(config-cmd): add set command to set space

---
+++ cmd/set.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/helltf/typing-speed-cli/internal/config"
+	"github.com/spf13/cobra"
+)
+
+// setCmd represents the set command
+var setCmd = &cobra.Command{
+	Use:   "set",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		space, _ := cmd.Flags().GetString("space")
+
+		err := setSpaceChar(space)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Successfully set space character")
+	},
+}
+
+func init() {
+	configCmd.AddCommand(setCmd)
+	setCmd.PersistentFlags().String("space", "", "Set your space character")
+}
+
+func setSpaceChar(char string) error {
+	err := config.SetSpace(char)
+
+	return err
+}
