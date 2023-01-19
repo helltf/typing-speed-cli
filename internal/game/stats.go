c4db8bb8a4dd99fb9583462f4b4e671bd953769a
helltf <dario.zachow5530@gmail.com>
2023-01-19 15:26:33 +0100

    feat: add stats for game

---
+++ internal/game/stats.go
@@ -0,0 +1,61 @@
+package game
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	"github.com/helltf/typing-speed-cli/internal/util"
+)
+
+type Stats struct {
+	Last    StatsData `json:"last"`
+	Average StatsData `json:"average"`
+}
+
+type StatsData struct {
+	Time  int     `json:"time"`
+	Words int     `json:"words"`
+	Cps   float64 `json:"cps"`
+}
+
+func GenerateStats(game *Game) *Stats {
+	return &Stats{
+		Last: StatsData{
+			Time:  game.time,
+			Words: game.words,
+			Cps:   game.Cps,
+		},
+		Average: getAverageStats(game),
+	}
+}
+
+func SaveStats(stats *Stats) error {
+	file, err := json.MarshalIndent(stats, "", " ")
+
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile("stats.json", file, 0644)
+}
+
+func ReadStats() *Stats {
+	path := "stats.json"
+
+	result, err := util.ReadJsonFile[Stats](path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+func getAverageStats(game *Game) StatsData {
+	return StatsData{
+		Time:  1,
+		Words: 1,
+		Cps:   1,
+	}
+}
