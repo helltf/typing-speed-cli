eb81700c565771e22431bc1d255755c0b3613bc7
helltf <dario.zachow5530@gmail.com>
2023-01-20 09:31:47 +0100

    feat: save history of stats

--- internal/game/stats.go
+++ internal/game/stats.go
@@ -8,24 +8,33 @@ import (
 )
 
 type Stats struct {
-	Last    StatsData `json:"last"`
-	Average StatsData `json:"average"`
+	Last    StatsData     `json:"last"`
+	Average *StatsData    `json:"average"`
+	History *StatsHistory `json:"history"`
+}
+
+type StatsHistory struct {
+	Time  []int `json:"time"`
+	Words []int `json:"words"`
+	Cps   []int `json:"cps"`
 }
 
 type StatsData struct {
-	Time  int     `json:"time"`
-	Words int     `json:"words"`
-	Cps   float64 `json:"cps"`
+	Time  int `json:"time"`
+	Words int `json:"words"`
+	Cps   int `json:"cps"`
 }
 
 func GenerateStats(game *Game) *Stats {
+	avr, history := getAverageStats(game)
 	return &Stats{
 		Last: StatsData{
 			Time:  game.time,
 			Words: game.words,
 			Cps:   game.Cps,
 		},
-		Average: getAverageStats(game),
+		Average: avr,
+		History: history,
 	}
 }
 
@@ -51,10 +60,48 @@ func ReadStats() (*Stats, error) {
 	return result, nil
 }
 
-func getAverageStats(game *Game) StatsData {
-	return StatsData{
-		Time:  1,
-		Words: 1,
-		Cps:   1,
+func getAverageStats(game *Game) (*StatsData, *StatsHistory) {
+	stats, err := ReadStats()
+
+	if err != nil {
+		return &StatsData{
+				Time:  game.time,
+				Words: game.words,
+				Cps:   game.Cps,
+			},
+			&StatsHistory{
+				Cps:   []int{},
+				Time:  []int{},
+				Words: []int{},
+			}
 	}
+	timeAvr, timeHistory := getAverageAndHistory(stats.History.Time, game.time)
+	cpsAvr, cpsHistory := getAverageAndHistory(stats.History.Cps, game.Cps)
+	wordsAvr, wordsHistory := getAverageAndHistory(stats.History.Words, game.words)
+
+	return &StatsData{
+			Time:  timeAvr,
+			Cps:   cpsAvr,
+			Words: wordsAvr,
+		},
+		&StatsHistory{
+			Words: wordsHistory,
+			Cps:   cpsHistory,
+			Time:  timeHistory,
+		}
+}
+
+func getAverageAndHistory(slice []int, additional int) (int, []int) {
+	history := append(slice, additional)
+	sum := 0
+
+	if len(history) >= 10 {
+		history = history[1:]
+	}
+
+	for _, v := range history {
+		sum += v
+	}
+
+	return sum / len(history), history
 }
